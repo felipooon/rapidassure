1274ec5f6d9b783ffc774a83a37b0940e3bcf01a
Felipe Godoy <felipegp90@gmail.com>
2026-09-16 20:11:51 -0300

    Fix staticfiles collection and manifest error for Render deployment

--- rapidassure_app/settings.py
+++ rapidassure_app/settings.py
@@ -177,7 +177,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -219,10 +219,9 @@ else:
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
         },
         "staticfiles": {
-            "BACKEND": "whitenoise.storage.ManifestStaticFilesStorage",
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
         },
     }
-    WHITENOISE_MANIFEST_STRICT = False
 
 
 """
